18b3918bfd0b30efb0d40299ad74dc6850208231
kpw6 <kpw6@njit.edu>
2021-04-04 23:08:59 -0400

    Level 1

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+     private float moveSpeed = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            transform.position += moveSpeed * new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        }
+
+    }
+}
---
+++ Assets/Scripts/OnClick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClick : MonoBehaviour
+{
+    public GameObject UI, player, target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0)  && Vector3.Distance(target.transform.position, player.transform.position) < 3.0f )
+        {
+            UI.SetActive(true);
+        }
+    }
+}
